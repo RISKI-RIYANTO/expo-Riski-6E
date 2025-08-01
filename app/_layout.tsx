9be8b3034880d01f721027068fe9494f7ed2a660
Riski RA <139326949+RISKI-RIYANTO@users.noreply.github.com>
2025-08-01 23:45:21 +0700

    layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,9 @@
-// import { Stack } from "expo-router";
-
-// export default function RootLayout() {
-//   return <Stack />;
-// }
 import { Stack } from "expo-router";
 
-// Komponen layout utama untuk navigasi aplikasi
 export default function AppLayout() {
   return (
     <Stack>
-      {/* Menyembunyikan header untuk navigasi tab */}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
     </Stack>
   );
 }
